83eff2ff4ad4d6196b828a3ea64cd28eabcc58d4
harshada2576 <harshada2576@gmail.com>
2025-12-11 19:41:47 +0530

    updated

--- power.c
+++ power.c
@@ -1,18 +1,19 @@
-#include<stdio.h>
-void main()
+#include <stdio.h>
+int power(int x, int n)
 {
-	int x,n,result;
-	printf("Enter base:");
-	scanf("%d",&x);
-	printf("Enter exponent:");
-	scanf("%d",&n);
-	result=power(x,n);
-	printf("%d raise to %d is :%d",x,n,result);
-}
-int power(int x,int n)
-{
-	if(n==0)
-	return 1;
+	if (n == 0)
+		return 1;
 	else
-	return x*power(x,n-1);
+		return x * power(x, n - 1);
+}
+int main()
+{
+	int x, n, result;
+	printf("Enter base:");
+	scanf("%d", &x);
+	printf("Enter exponent:");
+	scanf("%d", &n);
+	result = power(x, n);
+	printf("%d raise to %d is :%d", x, n, result);
+	return 0;
 }
