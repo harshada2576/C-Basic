ad3b336aaac2bcf3b88d563c094184565ed524dd
harshada2576 <harshada2576@gmail.com>
2026-02-21 21:12:33 +0530

    factorial using function

---
+++ factorial_using_function.c
@@ -0,0 +1,36 @@
+// factorial_using_function.c
+#include <stdio.h>
+
+// Function to calculate factorial
+long long factorial(int n)
+{
+    long long fact = 1;
+    int i;
+
+    for (i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+
+    return fact;
+}
+
+int main(void)
+{
+    int num;
+
+    // Take input from user
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num < 0)
+    {
+        printf("Factorial is not defined for negative numbers.\n");
+    }
+    else
+    {
+        printf("Factorial of %d is: %lld\n", num, factorial(num));
+    }
+
+    return 0;
+}
